f98ce24bbc2e3dbea17059e024571198ae117452
renatolins <renatolinsren@gmail.com>
2021-03-07 19:26:49 -0300

    add graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,29 @@
+import LinkWrapper from 'components/LinkWrapper'
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
+
 import dynamic from 'next/dynamic'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
   return (
-    <Map
-      places={[
-        {
-          id: '1',
-          name: 'Reykjavik',
-          slug: 'reykjavik',
-          location: {
-            latitude: -20,
-            longitude: -50
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} aria-label="About" />
+      </LinkWrapper>
+      <Map
+        places={[
+          {
+            id: '1',
+            name: 'Reykjavik',
+            slug: 'reykjavik',
+            location: {
+              latitude: -20,
+              longitude: -50
+            }
           }
-        }
-      ]}
-    />
+        ]}
+      />
+    </>
   )
 }
